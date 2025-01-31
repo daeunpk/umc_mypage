7c2fd5a7ea0268cff9b20a1422da5ed0d634b383
daeunpk <165495816+daeunpk@users.noreply.github.com>
2025-01-31 11:14:19 +0900

    myrecipes/logout page is being edited

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'pretendard/dist/web/static/pretendard.css';
-import ProfilePage from './pages/ProfilePage';
-import ProfileEditPage from './pages/ProfileEditPage';
+// import ProfilePage from './pages/ProfilePage';
+// import ProfileEditPage from './pages/ProfileEditPage';
+// import SignupForm from './pages/signupForm';
+import MyRecipe from './pages/SlideContent/MyRecipes';
+import DeleteAccountNextPage from './pages/DeleteAccountNextpage';
+import DeleteAccount from './pages/DeleteAccount';
+// import DeleteAccount from './pages/DeleteAccount';
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<ProfilePage />} />
-                <Route path="/edit-profile" element={<ProfileEditPage/>}/>
+                {/* <Route path="/" element={<ProfilePage/>}/> */}
+                <Route path="/" element={<MyRecipe/>}/>
+                <Route path="/mypage/delete-account" element={<DeleteAccount/>}/>
+                <Route path="/mypage/delete-account-next" element={<DeleteAccountNextPage />} />
             </Routes>
         </Router>
     );
